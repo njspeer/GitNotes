1fa274beb6d253c7c550015a005ad531122be7cf
njspeer <njspeer@gmail.com>
2021-04-05 11:17:58 -0500

    update to notes

--- gitnotes.jl
+++ gitnotes.jl
@@ -47,4 +47,76 @@
         Click Add SSH key.
 """
 
+# initialize a new repository
+"""
+5) git init
+   git add .
+   git commit
+   git push
+"""
+
+# Or clone an existing repository
+"""
+6) git clone https://github.com/libgit2/libgit2
+   git clone https://github.com/libgit2/libgit2 mylibgit # clones same repository into a dir named 'mylibgit'
+"""
+
+# Checking the Status of Your Files
+"""
+The main tool you use to determine which files are in which state is the git status command. If you run this command directly after a clone, you should see something like this:
+→ git status
+"""
+
+# Ignoring Files
+"""
+Often, you’ll have a class of files that you don’t want Git to automatically add or even show you as being untracked. 
+These are generally automatically generated files such as log files or files produced by your build system. 
+In such cases, you can create a file listing patterns to match them named .gitignore. Here is an example .gitignore file:
+
+→ cat .gitignore
+*.[oa]
+*~
+
+The first line tells Git to ignore any files ending in “.o” or “.a” — object and archive files that may be the product of building your code. 
+The second line tells Git to ignore all files whose names end with a tilde (~), which is used by many text editors such as Emacs to mark temporary files.
+
+The rules for the patterns you can put in the .gitignore file are as follows:
+
+  • Blank lines or lines starting with # are ignored.
+  • Standard glob patterns work, and will be applied recursively throughout the entire working tree.
+  • You can start patterns with a forward slash (/) to avoid recursivity.
+  • You can end patterns with a forward slash (/) to specify a directory.
+  • You can negate a pattern by starting it with an exclamation point (!).
+
+  Glob patterns are like simplified regular expressions that shells use. 
+  An asterisk (*) matches zero or more characters; [abc] matches any character inside the brackets (in this case a, b, or c); 
+  a question mark (?) matches a single character; 
+  and brackets enclosing characters separated by a hyphen ([0-9]) matches any character between them (in this case 0 through 9). 
+  You can also use two asterisks to match nested directories; a/**/z would match a/z, a/b/z, a/b/c/z, and so on.
+
+  for a large list of examples, see here:  https://github.com/github/gitignore
+"""
+
+# To see what chages you've made in more detail
+"""
+→ git diff
+→ git diff --staged
+→ git diff --cached # same as --staged
+
+Note:
+We will continue to use the git diff command in various ways throughout the rest of the book. 
+There is another way to look at these diffs if you prefer a graphical or external diff viewing program instead. 
+If you run git difftool instead of git diff, you can view any of these diffs in software like emerge, vimdiff and many more (including commercial products). 
+Run git difftool --tool-help to see what is available on your system.
+"""
+
+# Committing Your Changes
+"""
+→ git add .
+→ git commit # then add comments in editor
+→ git commit -m "add comments in one line"
+"""
+
+
+
 
